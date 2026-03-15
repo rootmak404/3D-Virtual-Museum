2baa8b61c07714df9246bb145429502736fb94da
Mohsin Ali Khan <khanmohsinali029@gmail.com>
2026-03-15 19:38:55 +0530

    remove LoginSignup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import MuseumScene from "./MuseumScene";
-import LoginSignup from "./LoginSignup";
 import SubscriptionPage from "./SubscriptionPage";
 
 function App() {
