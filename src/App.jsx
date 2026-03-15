eb2b3b2e31bdd90f212bb0150f93ba4c5f63a3a0
Mohsin Ali Khan <khanmohsinali029@gmail.com>
2026-03-15 19:43:05 +0530

    remove Subscription component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import MuseumScene from "./MuseumScene";
-import SubscriptionPage from "./SubscriptionPage";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/museum" element={<MuseumScene />} />
-        <Route path="/subscribe" element={<SubscriptionPage />} />
       </Routes>
     </Router>
   );
